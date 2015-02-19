ea105e3d3c144a790cec8be8b16c660d2bfe789a
bruninoit <brunino98@hotmail.it>
2015-02-19 17:41:04 +0100

    Create common.php

---
+++ it/common.php
@@ -0,0 +1,20 @@
+<?php
+/** 
+* 
+* @package StaffIt - Top Ten Topics 
+* @copyright (c) 2014 brunino
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
+* 
+*/ 
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+$lang = array_merge($lang, array(
+	'WVTT_TITLE'			=> 'Chi ha visto questo topic',
+    'EXTENSION_BY'			=> 'Estensione creata da',
+));
