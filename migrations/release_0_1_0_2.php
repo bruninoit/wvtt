c33f3e998d18d90c26f844774909b6ba82368be5
bruninoit <brunino98@hotmail.it>
2015-02-21 15:06:26 +0100

    new column

---
+++ migrations/release_0_1_0_2.php
@@ -0,0 +1,26 @@
+<?php
+/** 
+* 
+* @package Who Visit This Topic
+* @copyright (c) 2014 brunino
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
+* 
+*/ 
+namespace bruninoit\wvtt\migrations;
+class release_0_1_0 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\phpbb\db\migration\data\v310\gold');
+	}
+public function update_schema()
+{
+    return array(
+        'add_columns'        => array(
+            $this->table_prefix . 'wvtt'        => array(
+                'date'    => array('UINT', 0, 'after' => 'topic_id'),
+            )
+        ),
+    );
+}
+}
