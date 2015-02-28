1340763c05c67924d8036dc1c82bced3d78f5b47
bruninoit <brunino98@hotmail.it>
2015-02-28 18:08:02 +0100

    Update and rename release_0_1_0_4.php to release_0_1_0_b1.php

--- migrations/release_0_1_0_b1.php
+++ migrations/release_0_1_0_b1.php
@@ -7,7 +7,7 @@
 * 
 */ 
 namespace bruninoit\wvtt\migrations;
-class release_0_1_0_4 extends \phpbb\db\migration\migration
+class release_0_1_0_b1 extends \phpbb\db\migration\migration
 {
 	static public function depends_on()
 	{
@@ -16,9 +16,9 @@ class release_0_1_0_4 extends \phpbb\db\migration\migration
 	public function update_schema()
 	{
 	return array(
-	array('permission.add', array('u_wvtt')),
-	array('permission.permission_set', array('REGISTERED', 'u_wvtt', 'group')),
-	array('permission.permission_set', array('ROLE_USER_STANDARD', 'u_wvtt')),
+	array('permission.add', array('u_wvtt'), true),
+	array('permission.permission_set', array('REGISTERED', 'u_wvtt', 'group', true)),
+	array('permission.permission_set', array('ROLE_USER_STANDARD', 'u_wvtt', 'rule', true)),
   );
 }
 }
