81a9c7e82308ab0fe6af0911f98458c1f788b1bf
bruninoit <brunino98@hotmail.it>
2015-03-08 22:29:40 +0100

    profile permission

--- migrations/release_0_1_0_rc2.php
+++ migrations/release_0_1_0_rc2.php
@@ -20,6 +20,10 @@ class release_0_1_0_rc2 extends \phpbb\db\migration\migration
 	array('permission.permission_set', array('REGISTERED', 'u_wvtt_count', 'group', true)),
 	array('permission.permission_set', array('ROLE_USER_STANDARD', 'u_wvtt_count', 'rule', true)),
 	array('permission.permission_set', array('ROLE_ADMIN_FULL', 'u_wvtt_count', 'rule', true)),
+	array('permission.add', array('u_wvtt_profile', true)),
+	array('permission.permission_set', array('REGISTERED', 'u_wvtt_profile', 'group', true)),
+	array('permission.permission_set', array('ROLE_USER_STANDARD', 'u_wvtt_profile', 'rule', true)),
+	array('permission.permission_set', array('ROLE_ADMIN_FULL', 'u_wvtt_profile', 'rule', true)),
   );
 }
 }
