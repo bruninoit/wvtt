0568df642299dfefd18c20f8d79006b733ea4bf7
jakubsuchybio <jakubsuchybio@outlook.com>
2015-03-19 00:05:16 +0100

    Typo in wvtt_title. Add wvtt_details (used in another pull request).

--- language/cs/common.php
+++ language/cs/common.php
@@ -15,11 +15,12 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 $lang = array_merge($lang, array(
-	'WVTT_TITLE'			=> 'Kdo navštívil toto téma.',
+	'WVTT_TITLE'			=> 'Kdo navštívil toto téma',
 	'WVTT_PROFILE'			=> 'Poslední navštívené témata',
-        'EXTENSION_BY'			=> 'Doplněk od',
-        'ACL_U_WVTT'			=> 'Může vidět Kdo navštívil toto téma',
-        'ACL_U_WVTT_POPUP'		=> 'Může vidět datumy těch, kdo navštívili téma',
-        'ACL_U_WVTT_COUNT'		=> 'Může vidět počty navštívení tématu pro každého, kdo navštívil téma',
-        'ACL_U_WVTT_PROFILE'		=> 'Může vidět poslední navštívené témata uživatelů v jejich profilu'
+    'EXTENSION_BY'			=> 'Doplněk od',
+    'ACL_U_WVTT'			=> 'Může vidět Kdo navštívil toto téma',
+    'ACL_U_WVTT_POPUP'		=> 'Může vidět datumy těch, kdo navštívili téma',
+    'ACL_U_WVTT_COUNT'		=> 'Může vidět počty navštívení tématu pro každého, kdo navštívil téma',
+    'ACL_U_WVTT_PROFILE'	=> 'Může vidět poslední navštívené témata uživatelů v jejich profilu',
+    'WVTT_DETAILS'          => '--Zobrazit detaily--'
 ));
