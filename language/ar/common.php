f2614726d7274b47ef1a3e084dd64441c5fb486e
Raphaël M <contact@galixte.com>
2015-10-03 01:02:17 +0200

    Update language + fix PB styles
    Hi @b
    
    could you merge this, please ?
    
    - update all languages for adding a new language specific to PB styles
    (from PayBas),
    - adds support for new style (PBWoW 3),
    - fixes support for PBTech style.

--- language/ar/common.php
+++ language/ar/common.php
@@ -1,26 +1,50 @@
 <?php
-/** 
-* 
-* @package Who Visit This Topic 
-* @copyright (c) 2014 brunino
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
-* 
-* Translated By : Bassel Taha Alhitary - www.alhitary.net
-*/ 
+/**
+*
+* Who Visited This Topic extension for the phpBB Forum Software package.
+* Arabic translation by Bassel Taha Alhitary (http://www.alhitary.net)
+*
+* @copyright (c) 2015 brunino
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
 if (!defined('IN_PHPBB'))
 {
 	exit;
 }
+
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
-	'WVTT_TITLE'			=> 'من قرأ هذا الموضوع',
-	'WVTT_PROFILE'			=> 'آخر المواضيع التي قراءها',
-        'EXTENSION_BY'			=> 'الإضافة بواسطة ',
-        'ACL_U_WVTT'			=> 'يستطيع مُشاهدة الإضافة : من قرأ هذا الموضوع',
-        'ACL_U_WVTT_POPUP'		=> 'يستطيع مُشاهدة التاريخ في الإضافة : من قرأ هذا الموضوع',
-        'ACL_U_WVTT_COUNT'		=> 'يستطيع مُشاهدة عدد مشاهدات الموضوع لكل عضو',
-        'ACL_U_WVTT_PROFILE'		=> 'يستطيع مُشاهدة "آخر المواضيع التي قراءها" (في الملف الشخصي)'
+	'ACL_U_WVTT'		=> 'يستطيع مُشاهدة الإضافة : من قرأ هذا الموضوع',
+	'ACL_U_WVTT_COUNT'		=> 'ييستطيع مُشاهدة عدد مشاهدات الموضوع لكل عضو',
+	'ACL_U_WVTT_POPUP'		=> 'ييستطيع مُشاهدة التاريخ في الإضافة : من قرأ هذا الموضوع',
+	'ACL_U_WVTT_PROFILE'		=> 'ييستطيع مُشاهدة "آخر المواضيع التي قراءها" (في الملف الشخصي)',
+	'EXTENSION_BY'		=> 'الإضافة بواسطة',
+	'WVTT_DETAILS'		=> 'انظر التفاصيل',
+	'WVTT_PROFILE'		=> 'آخر المواضيع التي قراءها',
+	'WVTT_TITLE'		=> 'من قرأ هذا الموضوع',
 ));
--- language/en/common.php
+++ language/en/common.php
@@ -1,25 +1,49 @@
 <?php
-/** 
-* 
-* @package Who Visit This Topic 
-* @copyright (c) 2014 brunino
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
-* 
-*/ 
+/**
+*
+* Who Visited This Topic extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2015 brunino
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
 if (!defined('IN_PHPBB'))
 {
 	exit;
 }
+
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
-	'WVTT_TITLE'			=> 'Who Visited This Topic',
-	'WVTT_PROFILE'			=> 'Last topics visited',
-        'EXTENSION_BY'			=> 'Extension By',
-        'ACL_U_WVTT'			=> 'Can view Who Visited A Topic',
-        'ACL_U_WVTT_POPUP'		=> 'Can view the dates of who visited a topic',
-        'ACL_U_WVTT_COUNT'		=> 'Can view topics visits number for each user',
-        'ACL_U_WVTT_PROFILE'		=> 'Can view last topics visited by users (in profiles)'
+	'ACL_U_WVTT'		=> 'Can view Who Visited A Topic',
+	'ACL_U_WVTT_COUNT'		=> 'Can view topics visits number for each user',
+	'ACL_U_WVTT_POPUP'		=> 'Can view the dates of who visited a topic',
+	'ACL_U_WVTT_PROFILE'		=> 'Can view last topics visited by users (in profiles)',
+	'EXTENSION_BY'		=> 'Extension created by',
+	'WVTT_DETAILS'		=> 'See details',
+	'WVTT_PROFILE'		=> 'Last topics visited',
+	'WVTT_TITLE'		=> 'Who Visited This Topic',
 ));
